b1837aebb43b994089af6eaeb8f18798fd80f674
porapot <porapot2@gmail.com>
2019-07-05 17:26:33 +0700

    กึ่งสำเร็จรูป

--- src/app/add-student-dialog/add-student-dialog.component.ts
+++ src/app/add-student-dialog/add-student-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-add-student-dialog',
@@ -7,13 +8,16 @@ import { MatDialogRef } from '@angular/material';
   styleUrls: ['./add-student-dialog.component.scss']
 })
 export class AddStudentDialogComponent implements OnInit {
+  isLinear = false;
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+constructor(public dialogRef: MatDialogRef<DialogOverviewExampleDialog>, ) { }
 
-  constructor(public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,) { }
 
   ngOnInit() {
   }
   onNoClick(): void {
     this.dialogRef.close();
-    
+
   }
 }
