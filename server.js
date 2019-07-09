fdb8e0cf1b6d695ec28282b8195d16ef70c99fa8
porapot <porapot2@gmail.com>
2019-07-09 17:38:04 +0700

    deploy

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-const express = request('express');
+//Install express server
+const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/schoolapp'));
 
-app.listen(process.env.PORT || 8080);
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname,'/dist/schoolapp/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 3100);
